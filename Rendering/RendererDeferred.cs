f2259d9f233b90ad14901cc8ce7b71a65b473c63
BlueTRaven <Taylor@adoniram.net>
2022-09-12 16:49:27 -0400

    Start of Deferred

---
+++ Rendering/RendererDeferred.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViMG.Rendering
+{
+    public class RendererDeferred
+    {
+        private readonly GraphicsDevice device;
+
+        public RendererDeferred(GraphicsDevice device)
+        {
+            this.device = device;
+        }
+
+        public void SetPipelineState()
+        {
+
+        }
+
+        public void Draw()
+        {
+            SetPipelineState();
+
+            //GBuffer RTS
+            device.SetRenderTargets();
+        }
+    }
+}
