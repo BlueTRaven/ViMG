5e291076e19beddc9da3713d030e8b2fde315543
BlueTRaven <Taylor@adoniram.net>
2023-02-26 21:01:54 -0500

    Cryptstone blocks

---
+++ Cubes/CubeCryptCobbles.cs
@@ -0,0 +1,19 @@
+﻿using BrUtility;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViMG.Cubes
+{
+    public class CubeCryptCobbles : Cube
+    {
+        public CubeCryptCobbles() : base("stone_crypt_cobbles", new RectangleF(128, 96, 16, 16), Color.White, 3, 1)
+        {
+            Name = "Crypt Cobbles";
+            Description = "Perhaps once used for paving the ground of crypts.";
+        }
+    }
+}
---
+++ Cubes/CubeCryptStone.cs
@@ -0,0 +1,27 @@
+﻿using BrUtility;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViMG.Items;
+
+namespace ViMG.Cubes
+{
+    public class CubeCryptStone : Cube
+    {
+        public CubeCryptStone() : base("stone_crypt", new RectangleF(112, 96, 16, 16), Color.White, 3, 1)
+        {
+            Name = "Crypt Stone";
+            Description = "Dusty and old, this stone seems to bear many shards and fragments of bone.";
+        }
+
+        public override void GetDrops(List<ItemInstance> itemsToDrop)
+        {
+            base.GetDrops(itemsToDrop);
+
+            DropSelf(itemsToDrop);
+        }
+    }
+}
